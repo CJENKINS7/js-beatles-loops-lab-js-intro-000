ca187f1cdd5852a67ed77d54881096035d6ad91c
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-18 02:47:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
 var array = [];
+var action = 'plays'
 for (var i = 0; i < musicians.length; i++) {
   array.push(musicians[i] + "plays" + instruments[i]);
   }
